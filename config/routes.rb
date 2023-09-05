43f356cfab81fc6bcffa174509611d56341fb31e
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-09-05 03:05:37 +0000

    deviseの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 会員用
+  devise_for :members, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 end
