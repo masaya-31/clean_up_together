cf603b722369e8e773549889c0bcd50e236d96e8
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-09-06 06:53:43 +0000

    会員メールアドレス変更機能作成

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -20,22 +20,32 @@ class Public::MembersController < ApplicationController
   end
 
   def email_edit
+    @member = current_member
   end
 
   def email_update
-
+    @member = current_member
+    if @member.update(member_email_params)
+      redirect_to edit_member_path(@member)
+    else
+      render :email_edit
+    end
   end
 
-  def password_edit
-  end
+  # def password_edit
+  # end
 
-  def password_update
+  # def password_update
 
-  end
+  # end
 
   private
 
   def member_profile_params
     params.require(:member).permit(:name, :introduction)
+  end
+
+  def member_email_params
+    params.require(:member).permit(:email)
   end
 end
