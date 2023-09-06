8d7574493f4d4573178b016fe31190c44379ce5c
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-09-06 02:18:51 +0000

    マイページのサイドバー作成

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,5 +1,6 @@
 class Public::MembersController < ApplicationController
   def show
+    @member = Member.find(params[:id])
   end
 
   def follows
@@ -7,22 +8,22 @@ class Public::MembersController < ApplicationController
 
   def edit
   end
-  
+
   def update
-    
+
   end
 
   def email_edit
   end
-  
+
   def email_update
-    
+
   end
 
   def password_edit
   end
-  
+
   def password_update
-    
+
   end
 end
