1db17e46053ad118d31f0765544b81f54a108271
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-09-08 04:28:21 +0000

    管理者：会員情報編集画面作成

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -3,9 +3,19 @@ class Admin::MembersController < ApplicationController
     @members = Member.all
   end
 
-  def show
+  def edit
+    @member = Member.find(params[:id])
   end
 
-  def edit
+  def update
+    member = Member.find(params[:id])
+    member.update(member_params)
+    redirect_to admin_members_path
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :introduction, :email, :is_active)
   end
 end
