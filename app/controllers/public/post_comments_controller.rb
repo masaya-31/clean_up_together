b983d24ffb0e3e66952857f285a60ae7c20827cf
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-09-11 06:25:08 +0000

    コメント機能の非同期化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,15 +1,15 @@
 class Public::PostCommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    comment = current_member.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to request.referer
+    @comment = current_member.post_comments.new(post_comment_params)
+    @comment.post_id = post.id
+    @comment.save
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to request.referer
+    @comment = PostComment.find(params[:id])
+    @comment.destroy
+    # PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
   end
 
   private
