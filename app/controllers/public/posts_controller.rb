8a20a046b179969fb4600bca68a132147cc13852
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-09-13 04:51:51 +0000

    非公開記事を他人が閲覧できないように設定

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -5,14 +5,14 @@ class Public::PostsController < ApplicationController
   def index
     # キーワード検索の時
     if params[:keyword].present?
-      @posts = Post.search(params[:keyword])
+      @posts = Post.search(params[:keyword]).published
     # タグ検索の時
     elsif params[:tag_id].present?
       @tag = Tag.find(params[:tag_id])
-      @posts = @tag.posts.order(created_at: :desc)
+      @posts = @tag.posts.order(created_at: :desc).published
     # 全ての投稿一覧
     else
-      @posts = Post.all.order(created_at: :desc)
+      @posts = Post.all.order(created_at: :desc).published
     end
     @tags = Tag.all
     # 投稿件数
