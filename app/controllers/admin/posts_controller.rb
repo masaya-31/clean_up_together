c51f6ac5048eb8ca629ab7477e9010771bf495d8
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-09-23 03:00:21 +0000

    管理者側のタグ検索機能修正

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,8 +1,22 @@
 class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
-    @posts = Post.all.published
+    # キーワード検索の時
+    if params[:keyword].present?
+      @posts = Post.search(params[:keyword]).published
+    # タグ検索の時
+    elsif params[:tag_id].present?
+      @tag = Tag.find(params[:tag_id])
+      @posts = @tag.posts.order(created_at: :desc).published
+    # 全ての投稿一覧
+    else
+      @posts = Post.all.order(created_at: :desc).published
+    end
+    @tags = Tag.all
+    # 投稿件数
+    @all_posts_count = @posts.count
+    @keyword = params[:keyword]
   end
 
   def show
