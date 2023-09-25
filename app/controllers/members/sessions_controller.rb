6a8e22ec8e490f478f3afe28945cb12d251926ed
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-09-25 06:29:52 +0000

    フラッシュメッセージの色変更

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -2,6 +2,7 @@ class Members::SessionsController < Devise::SessionsController
   def guest_sign_in
     member = Member.guest
     sign_in member
+    flash[:color] = "text-success"
     redirect_to member_path(current_member), notice: 'ゲストユーザーとしてログインしました。'
   end
 end
