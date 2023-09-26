dcbbb29db2cf7df7c3bd4c5f38947c67b9f98476
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-09-26 00:58:08 +0000

    管理者側の記事一覧に非公開を追加

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -5,10 +5,10 @@ class Admin::PostsController < ApplicationController
     # タグ検索の時
     if params[:tag_id].present?
       @tag = Tag.find(params[:tag_id])
-      @posts = @tag.posts.order(created_at: :desc).published.page(params[:page])
+      @posts = @tag.posts.order(created_at: :desc).page(params[:page])
     # 全ての投稿一覧
     else
-      @posts = Post.all.order(created_at: :desc).published.page(params[:page])
+      @posts = Post.all.order(created_at: :desc).page(params[:page])
     end
     @tags = Tag.all.limit(30)
     @favorited_tags = Tag.joins(:post_tags).group(:tag_id).order('count(post_id) desc')
