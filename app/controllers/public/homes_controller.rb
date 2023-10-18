1ca7176654440b03ee531b869a6d5dbe57e78877
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-10-18 05:02:46 +0000

    publicのhomes_controllerのコメント修正、追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,11 +2,10 @@ class Public::HomesController < ApplicationController
 
   def top
     @posts = Post.all.order(created_at: :desc).limit(6).published
-    # いいね数順に並べ替え
+    # いいね数順に記事の並べ替え
     @favorited_posts = Post.includes(:favorited_members).limit(6).published.sort {|a,b| b.favorited_members.size <=> a.favorited_members.size}
-    @tags = Tag.all.limit(30)
-    # 件数順に並び替え
-    @favorited_tags = Tag.joins(:post_tags).group(:tag_id).order('count(post_id) desc')
+    # 件数の順にタグを30件表示
+    @favorited_tags = Tag.joins(:post_tags).group(:tag_id).order('count(post_id) desc').limit(30)
   end
 
   def about
