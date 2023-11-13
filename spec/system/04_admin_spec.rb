c395c66bf36cba49c2b870ba12e3943578a9615b
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-11-13 12:11:50 +0000

    admin側のログイン前のURL確認テスト追加

---
+++ spec/system/04_admin_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe '管理者側のテスト' do
+  describe '管理者ログイン前のテスト' do
+    before do
+      visit new_admin_session_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/admin/sign_in'
+      end
+    end
+  end
+end
