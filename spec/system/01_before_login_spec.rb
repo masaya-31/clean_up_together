4077c1ae2ae18f609f991e7f2ef9aac658a0a136
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-11-14 12:32:51 +0000

    ヘッダーのテスト追加

--- spec/system/01_before_login_spec.rb
+++ spec/system/01_before_login_spec.rb
@@ -23,16 +23,22 @@ describe 'ユーザーログイン前のテスト' do
       end
     end
   end
-  
+
   describe 'About画面のテスト' do
     before do
       visit about_path
     end
-    
+
     context '表示内容の確認' do
       it 'URLが正しい' do
         expect(current_path).to eq '/about'
       end
+    end
+  end
+
+  describe 'ヘッダーのテスト' do
+    before do
+      visit root_path
     end
   end
 end
