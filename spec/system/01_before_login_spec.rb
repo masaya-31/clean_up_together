e816b35fe7d1830f7729fe0a8c5992160457a387
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-11-18 14:01:25 +0000

    会員の新規登録画面のRSpecテスト項目追加

--- spec/system/01_before_login_spec.rb
+++ spec/system/01_before_login_spec.rb
@@ -40,11 +40,41 @@ describe 'ユーザーログイン前のテスト' do
     before do
       visit root_path
     end
+
+    context '表示内容の確認' do
+      it 'ロゴ(Keep Clean House)がトップ画面へのリンクである' do
+        expect(page).to have_link '', href: root_path
+      end
+    end
   end
 
   describe 'ユーザーの新規登録テスト' do
     before do
       visit new_member_registration_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/members/sign_up'
+      end
+      it '「新規登録」と表示される' do
+        expect(page).to have_content '新規登録'
+      end
+      it 'nameフォームが表示される' do
+        expect(page).to have_field 'member[name]'
+      end
+      it 'emailフォームが表示される' do
+        expect(page).to have_field 'member[email]'
+      end
+      it 'passwordフォームが表示される' do
+        expect(page).to have_field 'member[password]'
+      end
+      it 'password_confirmationフォームが表示される' do
+        expect(page).to have_field 'member[password_confirmation]'
+      end
+      it '登録するボタンが表示される' do
+        expect(page).to have_button '登録する'
+      end
     end
   end
 end
